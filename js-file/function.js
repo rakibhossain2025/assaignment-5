fe721a8b914a41ae49004cee33e44744c1f6e78a
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-03-01 18:39:54 +0600

    some function added deferent way

--- js-file/function.js
+++ js-file/function.js
@@ -1,18 +1,53 @@
-document.getElementById("complete-btn1").addEventListener("click", function(){
-  click('complete-btn1', 'title-1','input1')
-})
-document.getElementById("complete-btn2").addEventListener("click", function(){
-  click('complete-btn2', 'title-2','input2')
-})
-document.getElementById("complete-btn3").addEventListener("click", function(){
-  click('complete-btn3', 'title-3','input3')
-})
-document.getElementById("complete-btn4").addEventListener("click", function(){
-  click('complete-btn4', 'title-4','input4')
-})
-document.getElementById("complete-btn5").addEventListener("click", function(){
-  click('complete-btn5', 'title-5','input5')
-})
-document.getElementById("complete-btn6").addEventListener("click", function(){
-  click('complete-btn6', 'title-6','input6')
+function functionClick(btn, boxTitle, inputField) {
+  let history = document.getElementById("history-add");
+  let text = document.getElementById(boxTitle).innerText;
+
+  let h = document.createElement("p");
+  h.innerText = `You have completed the task: ${text}`;
+  history.appendChild(h);
+  h.classList.add("history");
+
+  const button = document.getElementById(btn);
+  button.disabled = true;
+  button.style.backgroundColor = "gray";
+  button.innerText = "Completed";
+
+  const inputText = document.getElementById(inputField);
+    inputText.disabled = true;
+
+  alert(`You have completed: ${text}`);
+
+  let totalTask= document.getElementById("number-count");
+  let convertTask = parseInt(totalTask.innerHTML);
+  let totalCompleted= document.getElementById("total-completed");
+  let convertedTotalTask = parseInt(totalCompleted.innerHTML);
+
+  console.log( convertTask,convertedTotalTask)
+
+  convertTask--;
+  totalTask.innerText=convertTask
+  convertedTotalTask++;
+  totalCompleted.innerText=convertedTotalTask
+
+
+  totalClick++
+  if (totalClick === 6) {
+    alert("Congratulations! You have completed all tasks.");
+  }
+}
+let totalGivenTask = 6;
+let totalCompleted = 25;
+let totalClick = 0;
+for (let c = 1; c <= 6; c++) {
+  document.getElementById(`complete-btn${c}`).addEventListener("click", function () {
+      functionClick(`complete-btn${c}`, `title-${c}`, `input${c}`);
+    });
+}
+
+document.getElementById("clear-history").addEventListener("click", function(){
+  const div =document.getElementById("history-add");
+  const pTag =div.getElementsByTagName("p")
+  while (pTag.length >0){
+    pTag[0].remove()
+  }  
 })
