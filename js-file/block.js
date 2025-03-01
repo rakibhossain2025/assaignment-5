8af528d11958a8f235ebc568a9e80af9783e9219
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-03-01 11:46:43 +0600

    sd

--- js-file/block.js
+++ js-file/block.js
@@ -1,5 +1,5 @@
 document.getElementById("go-main").style.cursor="pointer"
 document.getElementById("go-main").style.color="white"
 document.getElementById("go-main").addEventListener("click", function(){
-  window.location.href ="../index.html";
+  window.location.href ="index.html";
 });
